0a0759d51600a289fb03c7f4cce35dafa8a3f945
Ghoshaya1 <ayan_gh1989@hotmail.com>
2021-06-24 01:39:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello1():
+def hello():
     rm_quote = lambda x: x.replace('"', '')
     df = pd.read_csv("/inmk/airtravel.csv",doublequote=False,converters={'\"1958\"': rm_quote,'\"1959\"': rm_quote,'\"1960\"': rm_quote})
     df = df.rename(columns=rm_quote)
