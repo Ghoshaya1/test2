fa434b092d4015c63b113189d5ad2179c09223bc
Ghoshaya1 <ayan_gh1989@hotmail.com>
2021-06-24 01:51:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def hello():
     df = pd.read_csv("/inmk/airtravel.csv",doublequote=False,converters={'\"1958\"': rm_quote,'\"1959\"': rm_quote,'\"1960\"': rm_quote})
     df = df.rename(columns=rm_quote)
     return df.to_html()
+@app.route("/rowcount")
+cf = pd.read_csv("/inmk/airtravel.csv",doublequote=False,converters={'\"1958\"': rm_quote,'\"1959\"': rm_quote,'\"1960\"': rm_quote})
+cf = index = cf.index
+number_of_rows = len(index)
+return 'The number of rows in the table is ', number_of_rows.to_html()
+
 if __name__ == '__main__':
     app.run()
