bc01eb8c951e02888e49a34edb7fe1bc62a03a9b
Ghoshaya1 <ayan_gh1989@hotmail.com>
2021-06-24 02:15:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def rowcount():
     cf = pd.read_csv("/inmk/airtravel.csv",doublequote=False,converters={'\"1958\"': rm_quote,'\"1959\"': rm_quote,'\"1960\"': rm_quote})
     cf = index = cf.index
     number_of_rows = len(index)
-    return 'The number of rows in the table is ', number_of_rows.to_html()
+    return number_of_rows.to_html()
          
 
 if __name__ == '__main__':
