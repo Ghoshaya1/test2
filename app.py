943a69a2bdb3d59aba30be18fb3a2b228a203155
Ghoshaya1 <ayan_gh1989@hotmail.com>
2021-06-24 00:45:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home1():
-    return "Hello, World!"
+def hello():
+    rm_quote = lambda x: x.replace('"', '')
+    df = pd.read_csv("/inmk/airtravel.csv",doublequote=False,converters={'\"1958\"': rm_quote,'\"1959\"': rm_quote,'\"1960\"': rm_quote})
+    df = df.rename(columns=rm_quote)
+    return df.to_html()
 if __name__ == '__main__':
     app.run()
